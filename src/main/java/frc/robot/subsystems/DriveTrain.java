a2800d15be53cd1e0f4f4aa8fbedbed9fc39e2fc
aric-volman <89823522+aric-volman@users.noreply.github.com>
2023-09-21 19:03:01 -0700

    Initial commit

---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,88 @@
+// Based upon 2021's Competition Season DriveTrain code
+
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+import com.kauailabs.navx.frc.AHRS;
+
+public class DriveTrain extends SubsystemBase 
+{
+  private final WPI_TalonSRX leftDriveTalon;
+  private final WPI_TalonSRX rightDriveTalon;
+
+  private AHRS navx = new AHRS(SPI.Port.kMXP);
+
+  private ShuffleboardTab DTTab = Shuffleboard.getTab("DriveTrain");
+  private GenericEntry LeftVoltage = DTTab.add("Left Voltage", 0.0).getEntry();
+  private GenericEntry RightVoltage = DTTab.add("Right Voltage", 0.0).getEntry();
+
+  /** Creates a new DriveTrain */
+  public DriveTrain() 
+  {
+    leftDriveTalon = new WPI_TalonSRX(Constants.DriveTrainPorts.LeftDriveTalonPort);
+    rightDriveTalon = new WPI_TalonSRX(Constants.DriveTrainPorts.RightDriveTalonPort);
+  
+    leftDriveTalon.setNeutralMode(NeutralMode.Coast);
+    rightDriveTalon.setNeutralMode(NeutralMode.Coast);
+
+    leftDriveTalon.setInverted(true);
+    rightDriveTalon.setInverted(false);
+
+    leftDriveTalon.setSensorPhase(true);
+    rightDriveTalon.setSensorPhase(true);
+
+    leftDriveTalon.configFactoryDefault();
+    leftDriveTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+    rightDriveTalon.configFactoryDefault();
+    rightDriveTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+
+  }
+
+  public void tankDrive(double leftSpeed, double rightSpeed) {
+    rightDriveTalon.set(rightSpeed);
+    leftDriveTalon.set(leftSpeed);
+  }
+
+  public void resetEncoders() {
+    leftDriveTalon.setSelectedSensorPosition(0,0,10);
+    rightDriveTalon.setSelectedSensorPosition(0,0,10);
+  }
+
+  public double getTicks() {
+    return (leftDriveTalon.getSelectedSensorPosition(0) + rightDriveTalon.getSelectedSensorPosition(0)) / 2.0;
+  }
+ 
+  public double getAngle(){
+    return navx.getAngle(); 
+  }
+ 
+  public void resetNavx(){
+    navx.reset();
+  }
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("Left Voltage", leftDriveTalon.getMotorOutputPercent());
+    SmartDashboard.putNumber("Right Voltage", rightDriveTalon.getMotorOutputPercent());
+    SmartDashboard.putNumber("Angle", navx.getAngle());
+
+    LeftVoltage.setDouble(leftDriveTalon.getMotorOutputPercent());
+    RightVoltage.setDouble(rightDriveTalon.getMotorOutputPercent());
+
+  }
+}
